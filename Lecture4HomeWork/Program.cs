89d058ab34b90fd876615625033135730f30b625
vlad <repa333@gmail.com>
2022-07-08 06:32:15 +0300

    Add project files.

---
+++ Lecture4HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture4HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
